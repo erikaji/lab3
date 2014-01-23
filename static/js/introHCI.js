5a0e172200a59291c70445722a6c8aac86685f30
vagrant <vagrant@precise32.(none)>
2014-01-23 08:41:07 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,36 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("Whoa, you clicked me!");	
+		$(".jumbotron p").toggleClass("active");
 	});
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	function projectClick(e) {
+   		 // Cancel the default action, which prevents the page from reloading
+    	e.preventDefault();
+	    // In an event listener, $(this) is the element that fired the event	    console.log("confirm1");
+	    var projectTitle = $(this).find("p").text();
+	    var jumbotronHeader = $(".jumbotron h1");
+	    jumbotronHeader.text(projectTitle);
+
+	    var containingProject = $(this).closest(".project");
+	    var description = $(containingProject).find(".project-description");
+		
+		if (description.length == 0) {
+			$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+		} else {
+	    	description.fadeOut();
+		}
+	}
+
+	$("#submitBtn").click(updateProject);
+}
+
+function updateProject(e){
+	var projectTitle = $('#project').val();
+	var newWidth = $('#width').val();
+	var newDesc = $('#description').val();
+	$(projectTitle).animate({width: newWidth});
+	$(projectTitle + " .project-description").text(newDesc);
 }
